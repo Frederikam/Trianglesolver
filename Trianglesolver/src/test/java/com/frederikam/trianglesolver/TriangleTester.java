2c83235d6354fba26315b8b9751a40377e35f139
Frederikam <frogkr@gmail.com>
2017-05-30 16:56:43 +0200

    Added more tests

--- Trianglesolver/src/test/java/com/frederikam/trianglesolver/TriangleTester.java
+++ Trianglesolver/src/test/java/com/frederikam/trianglesolver/TriangleTester.java
@@ -12,6 +12,13 @@ public class TriangleTester {
     private static final Logger log = LoggerFactory.getLogger(TriangleTester.class);
 
     @Test
+    void testAlreadyCompleted() {
+        Triangle input = new Triangle(60d, 60d, 60d, 5d, 5d, 5d);
+        Triangle output = new Triangle(60d, 60d, 60d, 5d, 5d, 5d);
+        testTriangle(input, output);
+    }
+
+    @Test
     void testInvalidAngleSum() {
         Triangle input = new Triangle(50d, 50d, 50d, null, null, null);
         testTriangle(input, null);
@@ -21,6 +28,45 @@ public class TriangleTester {
     void testAngleSum() {
         Triangle input = new Triangle(50d, 30d, null, null, null, null);
         Triangle target = new Triangle(50d, 30d, 100d, null, null, null);
+        testTriangle(input, target);
+    }
+
+    @Test
+    void testNoAngles() {
+        Triangle input = new Triangle(null, null, null, 133d, 185d, 190d);
+        Triangle target = new Triangle(41.51917d, 67.22587, 71.25496, 133d, 185d, 190d);
+        testTriangle(input, target);
+    }
+
+    @Test
+    void testNoInfo() {
+        Triangle input = new Triangle(null, null, null, null, null, null);
+        Triangle target = new Triangle(null, null, null, null, null, null);
+        testTriangle(input, target);
+    }
+
+    @Test
+    void test1Angle2Sides() {
+        Triangle input = new Triangle(null, 35d, null, null, 50d, 35d);
+        Triangle target = new Triangle(41.51917d, 67.22587, 71.25496, 133d, 185d, 190d);
+        testTriangle(input, target);
+
+        input = new Triangle(null, 35d, null, null, 35d, 50d);
+        target = new Triangle(89.97573d, 35d, 55.02427d, 61.02063d, 35d, 50d);
+        testTriangle(input, target);
+    }
+
+    @Test
+    void test2Angles1Side() {
+        Triangle input = new Triangle(50d, 60d, null, null, null, 5d);
+        Triangle target = new Triangle(50d, 60d, 70d, 5d, 5d, 5d);
+        testTriangle(input, target);
+    }
+
+    @Test
+    void testCosineAngles() {
+        Triangle input = new Triangle(50d, null, 70d, 5d, null, 5d);
+        Triangle target = new Triangle(50d, 60d, 70d, 5d, 5d, 5d);
         testTriangle(input, target);
     }
 
@@ -44,11 +90,11 @@ public class TriangleTester {
         // This is used to account for double precision
 
         if(d1 == null) {
-            Assertions.assertEquals(d1, d2);
+            Assertions.assertEquals(null, d2);
             return;
         }
 
-        Assertions.assertTrue(Math.abs(d1 - d2) < 0.0000001);
+        Assertions.assertTrue(Math.abs(d1 - d2) < 0.0001);
     }
 
 }
